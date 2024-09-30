38822e72e10967fc479f02f6f0ae565e318cda8c
Hyun7en <rhqudgus99@gmail.com>
2024-09-30 17:44:56 +0900

    2주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -7,20 +7,7 @@ public class Programmers {
     public static void main(String[] args) {
 
         String a = "hello";
-        String b = "";
-        int num1 = 1,num2 =2;
-        for(int i = 0; i < a.length(); i++) {
-            if( i == num1){
-                b+=a.charAt(num2);
-                continue;
-            }else if( i == num2){
-                b+=a.charAt(num1);
-                continue;
-            }
-            b += a.charAt(i);
-        }
 
 
-        System.out.println(b);
     }
 }
