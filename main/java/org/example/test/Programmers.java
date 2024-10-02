a6f5b8a59401f95d10549723f09a0bfad9beda7b
Hyun7en <rhqudgus99@gmail.com>
2024-10-02 16:56:46 +0900

    2주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -4,11 +4,7 @@ import java.util.StringTokenizer;
 
 public class Programmers {
     public static void main(String[] args) {
-       String a= "asdasgba";
 
-        for(int i  = 0 ; i < a.length(); i++){
-            System.out.print(a.charAt(i) + " ");
-        }
 
     }
 }
