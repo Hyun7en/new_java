57a5ee0c3d2ac95e9eac15cf2cc6b22879158c40
Hyun7en <rhqudgus99@gmail.com>
2024-10-02 16:37:11 +0900

    2주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -1,21 +1,14 @@
 package org.example.test;
 
+import java.util.StringTokenizer;
+
 public class Programmers {
     public static void main(String[] args) {
-        int age = 23;
+       String a= "asdasgba";
 
-        String age1 = String.valueOf(age);
-
-        for(int i = 0; i < age1.length(); i++){
-            age1.charAt(i);
+        for(int i  = 0 ; i < a.length(); i++){
+            System.out.print(a.charAt(i) + " ");
         }
-
-
-        int a = Character.getNumericValue('a');
-        int z = Character.getNumericValue('z');
-
-
-        System.out.println(a);
 
     }
 }
