7c04a4c45adf6543786b13451e279bd8f2e00562
Hyun7en <rhqudgus99@gmail.com>
2024-10-02 14:03:03 +0900

    2주차 교육 내용

---
+++ main/java/org/example/day5/array2/스트링기본.java
@@ -0,0 +1,7 @@
+package org.example.day5.array2;
+
+public class 스트링기본 {
+    public static void main(String[] args) {
+
+    }
+}
--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -6,8 +6,17 @@ import java.util.Arrays;
 public class Programmers {
     public static void main(String[] args) {
 
-        String a = "hello";
+        int k = 3;
+        int num = 13445;
+        int answer = 0;
+        String a = num + "";
 
+        for(int i =0; i < a.length(); i++){
+            if(a.charAt(i) == k){
+                answer = i+1;
+            }
+        }
 
+        System.out.println(answer);
     }
 }
