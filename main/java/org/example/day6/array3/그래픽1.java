1bb875f1004fbf85029a4ee50ebd78bb173a9541
Hyun7en <rhqudgus99@gmail.com>
2024-10-07 14:06:16 +0900

    3주차 교육 내용

--- main/java/org/example/day6/array3/그래픽1.java
+++ main/java/org/example/day6/array3/그래픽1.java
@@ -1,6 +1,7 @@
 package org.example.day6.array3;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class 그래픽1 {
     public static void main(String[] args) {
@@ -14,7 +15,27 @@ public class 그래픽1 {
         JButton btn = new JButton();
         btn.setText("나를 눌러!"); // 함수, 메서드, 명령어는 명령어(입력 데이터);
 
-        frame.add(btn);
+        JButton btn2 = new JButton();
+        btn2.setText("나를 눌러");
+        btn2.setBackground(Color.yellow);
+
+        JButton btn3 = new JButton();
+        btn3.setText("ㄹㄹ");
+        btn3.setBackground(Color.YELLOW);
+
+        JButton btn4 = new JButton();
+        btn4.setText("<<");
+        btn4.setBackground(Color.GREEN);
+
+        JButton btn5 = new JButton();
+        btn5.setText(">>");
+        btn5.setBackground(Color.GREEN);
+
+        frame.add(btn, BorderLayout.CENTER);
+        frame.add(btn2, BorderLayout.NORTH);
+        frame.add(btn3, BorderLayout.SOUTH);
+        frame.add(btn4, BorderLayout.WEST);
+        frame.add(btn5, BorderLayout.EAST);
 
         frame.setVisible(true);
         //end!
