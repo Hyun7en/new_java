13c7b4d85e7ac3795636bcdb282721a6b8130ee0
Hyun7en <rhqudgus99@gmail.com>
2024-10-07 09:15:32 +0900

    3주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -4,24 +4,7 @@ import java.util.StringTokenizer;
 
 public class Programmers {
     public static void main(String[] args) {
-        int n = 10;
-        int answer = 0;
-        for(int i = 4; i <= n; i ++){
-            int divisor = 0;
-            for(int j = 1; j <= i; j ++){
-
-                if(i%j == 0){
-                    divisor++;
-                }
-                if(divisor >= 3){
-                    answer++;
-                    break;
-                }
-            }
-
-        }
-
-        System.out.println(answer);
+      
 
     }
 }
