2ae33368e18d2e9cf1fc086a9266a440b47e018d
Hyun7en <rhqudgus99@gmail.com>
2024-10-08 14:19:04 +0900

    3주차 교육 내용

--- main/java/org/example/day7/클래스만들기/Television.java
+++
@@ -1,5 +0,0 @@
-package org.example.day7.클래스만들기;
-
-public class Television {
-    
-}
--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -3,8 +3,15 @@ package org.example.test;
 public class Programmers {
     public static void main(String[] args) {
 
+        int answer = 0;
+        String[] a= {"p","o","s"};
+        String[] dic = {"sod", "eocd", "qixm", "adio", "soo"};
 
+        for(String dic1 : dic){
+            dic1.split("");
+        }
 
+        System.out.println(answer);
         //end
     }
 }
