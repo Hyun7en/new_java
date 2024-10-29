005d1cfe1a4b0ff34cf3f0606c4fddbbfcb1e9e1
Hyun7en <rhqudgus99@gmail.com>
2024-10-29 17:50:10 +0900

    6주차 교육 내용

--- main/java/org/example/day21/문제풀이/배열컬렉션변환고급.java
+++ main/java/org/example/day21/문제풀이/배열컬렉션변환고급.java
@@ -1,18 +1,17 @@
 package org.example.day21.문제풀이;
 
+import java.util.Arrays;
+
 public class 배열컬렉션변환고급 {
     public static void main(String[] args) {
         String[] s3 = {"박명수", "유재석","김종국","송지효","하하"};
-        int ar = 134412;
-        String ar1 = Integer.toString(ar);
-        int k = 3;
-       String k1 = Integer.toString(k);
-       int index = ar1.indexOf(k1);
-        System.out.println(index);
+
 //        1. 오름차순 정렬해서 프린트
+        Arrays.sort(s3);
+        System.out.println(Arrays.toString(s3));
 
 //        2. 내림차순 정렬해서 프린트
-
+        
 //        3. 글자수가 3미만인 사람 삭제후  프린트
 
 //        4. 유재석을 찾아서 유재돌로 변경한 후 프린트
