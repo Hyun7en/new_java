adc8b0e96af62eeee924f31068a9f604234928f4
Hyun7en <rhqudgus99@gmail.com>
2024-10-29 10:27:10 +0900

    6주차 교육 내용

---
+++ main/java/org/example/day21/알고리즘/선형/배열원소길이.java
@@ -0,0 +1,20 @@
+package org.example.day21.알고리즘.선형;
+
+import java.util.Arrays;
+
+public class 배열원소길이 {
+    public static void main(String[] args) {
+        String[] n = {"We", "are", "the", "world!"};
+        Solution3 s = new Solution3();
+        int[] a = s.solution(n); //주소, 100번
+        System.out.println(Arrays.toString(a));
+    }
+}
+
+class Solution3 {
+    public int[] solution(String[] n) {
+        int[] answer = new int[n.length];
+
+        return answer;
+    }
+}
