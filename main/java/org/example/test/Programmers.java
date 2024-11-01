5d4e09e5b790a91c650ae4a54e937b507872eafd
Hyun7en <rhqudgus99@gmail.com>
2024-11-01 11:27:04 +0900

    6주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -5,12 +5,18 @@ import java.util.Arrays;
 
 public class Programmers {
     public static void main(String[] args) {
-        int[][] arr = {{1,4,3},{4,5,6}};
+        int[] answer = {};
+        int n = 12345;
+        String str = Integer.toString(n);
+        answer = new int[str.length()];
+        int i = 0;
+        while(n > 0){
+            answer[i++] = n%10;
+            n = n/10;
+            
+        }
 
-        Arrays.sort(arr[0]);
-
-        System.out.println(Arrays.toString(arr[0]));
-
+        System.out.println(Arrays.toString(answer));
 
         //end
     }
