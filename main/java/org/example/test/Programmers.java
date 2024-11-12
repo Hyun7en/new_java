3cd07a7a19ac9698da57d868cef28c13bf841a31
DESKTOP-95TOC3B\a <rhqudgus99@gmail.com>
2024-11-12 15:48:49 +0900

    본체 변경후 커밋 실험

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -5,17 +5,7 @@ import java.util.Arrays;
 
 public class Programmers {
     public static void main(String[] args) {
-        int[] answer = {};
-        int n = 12345;
-        String str = Integer.toString(n);
-        answer = new int[str.length()];
-        int i = 0;
-        while(n > 0){
-            answer[i++] = n%10;
-            n = n/10;
-
-        }
-
+        
 
         //end
     }
