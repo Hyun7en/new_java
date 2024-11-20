c1cad9b47581ba6928c8116c583d43e35a41e037
DESKTOP-GMDESLQ\a <rhqudgus99@gmail.com>
2024-11-20 11:43:05 +0900

    9주차 교육 내용

---
+++ main/java/org/example/mongo/Member여러개추가.java
@@ -0,0 +1,44 @@
+package org.example.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member여러개추가 {
+    public static void main(String[] args) {
+        //1. MongoClient 생성
+        MongoClient client = new MongoClient("localhost", 27017);
+        //2. db 연결
+        MongoDatabase db = client.getDatabase("shop");
+        MongoCollection<Document> member = db.getCollection("member");
+        System.out.println("member컬렉션까지 연결 성공!");
+
+        //3. 전송할 js 생성
+        //insert할 json(document)를 생성
+        Document doc = new Document();
+        doc.append("id", "apple2"); //{id : "apple"}
+        doc.append("pw", "1234"); //{ id : "apple", pw : "1234"}
+        doc.append("name", "apple2"); //{ id : "apple", pw : "1234", name : "apple"}
+        doc.append("tel", "011"); //{ id : "apple", pw : "1234", name : "apple", tel : "011"}
+
+        Document doc2 = new Document();
+        doc2.append("id", "apple3"); //{id : "apple"}
+        doc2.append("pw", "1234"); //{ id : "apple", pw : "1234"}
+        doc2.append("name", "apple3"); //{ id : "apple", pw : "1234", name : "apple"}
+        doc2.append("tel", "011"); //{ id : "apple", pw : "1234", name : "apple", tel : "011"}
+
+        List<Document> list = new ArrayList<>();
+        list.add(doc);
+        list.add(doc2);
+
+        //4. 전송, 결과처리
+        member.insertMany(list);
+        System.out.println("==== 몽고DB로 전송함.");
+        client.close();
+
+    }
+}
---
+++ main/java/org/example/mongo/Member하나추가2.java
@@ -0,0 +1,46 @@
+package org.example.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Scanner;
+
+public class Member하나추가2 {
+    public static void main(String[] args) {
+        //1. MongoClient 생성
+        MongoClient client = new MongoClient("localhost", 27017);
+        //2. db 연결
+        MongoDatabase db = client.getDatabase("shop");
+        MongoCollection<Document> member = db.getCollection("member");
+        System.out.println("member컬렉션까지 연결 성공!");
+        Document doc = new Document();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("id>> ");
+        String id = sc.next();
+
+        System.out.println("pw>> ");
+        String pw = sc.next();
+
+        System.out.println("name>> ");
+        String name = sc.next();
+
+        System.out.println("tel>> ");
+        String tel = sc.next();
+        sc.close();
+
+        //3. 전송할 js 생성
+        //insert할 json(document)를 생성
+        doc.append("id", id); //{id : "apple"}
+        doc.append("pw", pw); //{ id : "apple", pw : "1234"}
+        doc.append("name", name); //{ id : "apple", pw : "1234", name : "apple"}
+        doc.append("tel", tel); //{ id : "apple", pw : "1234", name : "apple", tel : "011"}
+
+        //4. 전송, 결과처리
+        member.insertOne(doc);
+        client.close();
+
+    }
+}
