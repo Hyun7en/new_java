539219bc364ff8f8cbe1018cfafcc983a5fe340d
DESKTOP-GMDESLQ\a <rhqudgus99@gmail.com>
2024-11-22 11:30:20 +0900

    9주차 교육 내용

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -1,9 +1,16 @@
 package org.example.test;
 
 
+import java.util.Random;
+
 public class Programmers {
     public static void main(String[] args) {
-        int   a =10;
+        int[] arr = new int[10];
+       Random rand = new Random();
+
+       for (int i = 0; i < 10; i++) {
+           arr[i] = rand.nextInt(100);
+       }
 
         //end
     }
