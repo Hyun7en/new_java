adb4474f9d594d13cff8094bb9d0be31c280f7ed
DESKTOP-GMDESLQ\a <rhqudgus99@gmail.com>
2024-11-25 12:27:22 +0900

    10주차 교육 내용

--- main/java/org/example/javafxTest/MainApp.java
+++ main/java/org/example/javafxTest/MainApp.java
@@ -1,24 +1,29 @@
 package org.example.javafxTest;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button("Hello, JavaFX!");
-        btn.setOnAction(e -> System.out.println("Button clicked"));
+    public void start(Stage primaryStage) throws Exception {
+        // fxml/testFX.fxml을 불러온다.
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getClassLoader().getResource("fxml/main.fxml")
+        ));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // GUI에 보일 프로그램 제목을 설정한다.
+        primaryStage.setTitle("Pickypal");
 
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("JavaFX with JDK 17");
-        primaryStage.setScene(scene);
+        // 화면을 설정한다. 두번째 파라미터는 프로그램의 Width, 세번째는 Height를 설정한다
+        primaryStage.setScene(new Scene(root, 800, 600));
+
+        // 화면을 보여주는 작업을 실행한다
         primaryStage.show();
     }
 
