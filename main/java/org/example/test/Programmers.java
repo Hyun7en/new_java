30377f71ddaa6cefbd299212a3cf9793e2e37b48
Hyun7en <rhqudgus99@gmail.com>
2024-12-16 11:08:32 +0900

    test

--- main/java/org/example/test/Programmers.java
+++ main/java/org/example/test/Programmers.java
@@ -1,17 +1,8 @@
 package org.example.test;
 
-
-import java.util.Random;
-
 public class Programmers {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-       Random rand = new Random();
-
-       for (int i = 0; i < 10; i++) {
-           arr[i] = rand.nextInt(100) + 1;
-           System.out.println(arr[i]);
-       }
+    
 
         //end
     }
